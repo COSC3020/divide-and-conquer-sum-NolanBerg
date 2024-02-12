62ff55505d4f336c8ec6d49a47f83b009a5bf57e
Nolan Berg <107907766+NolanBerg@users.noreply.github.com>
2024-02-12 13:49:31 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,14 @@
 function divideAndConquerSum(a) {
-    return a;
+return divideAndConquerSum(a, 0, a.length-1);
+}
+
+function divideAndConquerSum(a, low, high) {
+
+if(high == low)
+return a[low];
+
+var mid1 = low + (high - low)/3;
+var mid2 = mid1 + (high - low)/3;
+
+return divideAndConquerSum(a, low, mid1 - 1) + divideAndConquerSum(a, mid1 + 1, mid2 - 1) + divideAndConquerSum(a, mid2 + 1, high);
 }
