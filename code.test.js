0d3b11663ff13de3610d1f67144e21b500d5491e
Nolan Berg <107907766+NolanBerg@users.noreply.github.com>
2024-02-12 19:51:31 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -4,12 +4,6 @@ const { divideAndConquerSum } = require('./code.js');
 const testDivideAndConquerSum = jsc.forall("array nat", function (arr) {
     const expectedSum = arr.reduce((a, b) => a + b, 0);
     const calculatedSum = divideAndConquerSum(arr, 0, arr.length - 1);
-    
-    if (expectedSum !== calculatedSum) {
-        console.error(`Test failed for array: ${arr}`);
-        console.error(`Expected: ${expectedSum}, Calculated: ${calculatedSum}`);
-    }
-
     return expectedSum === calculatedSum;
 });
 
